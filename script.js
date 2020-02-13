d685299993b0ea083220f66eae915b1b493b727f
JesikaRenea <JessicaHayesUCF@outlook.com>
2020-02-12 21:29:51 -0500

    Added For Loops for Breweries and Restaurants

--- script.js
+++ script.js
@@ -24,10 +24,29 @@ searchBtn.on("click", function (event) {
         method: 'GET'
     })
         .then(function (res) {
-            //EMPTY DIV here $(".appendingDiv").empty(); //
+            $(".brew-display").empty();
 
-            console.log(res);
+            for (var i = 0; i < res.length; i++) {
+
+                var newBrewDiv = $("<div>").addClass("card");
+
+                var brewNameP = $("<h5>").text(res[i].name);
+                var brewTypeP = $("<p>").text("Brewery Type: " + res[i].brewery_type);
+                var brewAddressP = $("<p>").text("Address: " + res[i].street + ", " +res[0].city);
+                var brewPhoneP = $("<p>").text("Phone: " + res[i].phone);
+                var brewWebP = $("<a>").attr("href", res[i].website_url).attr("target", "_blank").text("Visit Website");
+                
+                newBrewDiv.append(brewNameP, brewTypeP, brewAddressP, brewPhoneP, brewWebP)
+                $(".brew-display").append(newBrewDiv);
+           
+            }
+
+
         });
+
+
+
+
 
 
 //GET's CITYID
@@ -53,7 +72,36 @@ searchBtn.on("click", function (event) {
                 "user-key": "5a33e1ec7d6535e6ef16c81e3833e48a"
             }
         }).then(function (res) {
-            console.log(res);
+            var restList = res.restaurants;
+            console.log(restList);
+            $(".rest-display").empty();
+
+            console.log(restList[0].restaurant.featured_image);
+            console.log(restList[0].restaurant.name);
+            console.log(restList[0].restaurant.cuisines)
+            console.log(restList[0].restaurant.location.address);
+            console.log(restList[0].restaurant.location.locality);
+            console.log(restList[0].restaurant.phone_numbers);
+            console.log(restList[0].restaurant.menu_url);
+
+
+            for (var i = 0; i < restList.length; i++) {
+
+                var newRestDiv = $("<div>").addClass("card");
+
+                var restImg = $("<img>").attr("src", restList[i].restaurant.featured_image).attr("alt text", "Featured Image");
+                var restNameP = $("<h5>").text(restList[i].restaurant.name);
+                var restCuisineP = $("<p>").text(restList[i].restaurant.cuisines);
+                var restAddressP = $("<p>").text("Address: " + restList[i].restaurant.location.address + ", Locality: " + restList[i].restaurant.location.locality);
+                var restPhoneP = $("<p>").text("Phone: " + restList[i].restaurant.phone_numbers);
+                var restMenu = $("<a>").attr("href", restList[i].restaurant.menu_url).attr("target", "_blank").text("View Menu");
+                
+                newRestDiv.append(restImg, restNameP, restCuisineP, restAddressP, restPhoneP, restMenu);
+                $(".rest-display").append(newRestDiv);
+           
+            }
+
+
         });
 
     });
@@ -61,7 +109,7 @@ searchBtn.on("click", function (event) {
 // TICKETMASTER Event Search
     $.ajax({
         type:"GET",
-        url:"https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=mGRhyVGMqSKLiGRVm4XLR9SJBSPsX0Eg&city=" + citySearch,
+        url:"https://app.ticketmaster.com/discovery/v2/events.json?size=20&apikey=mGRhyVGMqSKLiGRVm4XLR9SJBSPsX0Eg&city=" + citySearch,
         async:true,
         dataType: "json",
         success: function(json) {
