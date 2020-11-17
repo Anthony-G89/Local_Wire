e8c50b4b427d00f698951416dad64a1628425b37
Anthony Guerrero <adguerrero1935@gmail.com>
2020-11-17 18:01:11 -0500

    Created a button for user to click on and the page will go to the bottom of the page

--- assets/Scripts/btnFunction.js
+++ assets/Scripts/btnFunction.js
@@ -1,30 +1,48 @@
-topBtn = $("#mainBtn");
-
+topBtn = $("#topBtn");
+bottomBtn= $("#bottomBtn");
 
 $(document).ready(function (){
 
     topBtn.on("click", function () {
-        // window.onscroll = function () {scrollFunction()};
-
-
-        // function scrollFunction () {
-        //     if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        //         topBtn.style.display = "block";
-
-        //     }else {
-        //         topBtn.style.display = "none"
-        //     }
-        // }
-
-
         function topFunction () {
             document.documentElement.scrollTop = 0;
         }
 
         topFunction();
-
     })
 
+    bottomBtn.on("click", function () {
+        function bottomFunction () {
+            document.documentElement.scrollTop = 26000;
+        }
+
+        bottomFunction();
+    })
+
+});
+
+
+
+// window.onscroll = function () {scrollFunction() };
+
+
+//         function scrollFunction () {
+//             if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//                 topBtn.style.display = "block";
+
+//             }else {
+//                 topBtn.style.display = "none"
+//             }
+//         }
+
+
+// jQuery(window).scroll(function () {
+    //     var topButton = jQuery("#topBtn");
+    
+    //     if(jQuery(this).scrollTop() >= 200) topButton.addClass("shown");
+    //     else topButton.removeClass("shown");
+    // })
     
 
-})
+
+  
