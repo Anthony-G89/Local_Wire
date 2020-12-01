09d77d6b692b91661da77ca3dfb2844b7db0e976
Anthony Guerrero <adguerrero1935@gmail.com>
2020-12-01 13:18:16 -0500

    added a scroll function. buttons will appear when the user scrolls down

--- assets/Scripts/btnFunction.js
+++ assets/Scripts/btnFunction.js
@@ -1,25 +1,78 @@
-topBtn = $("#topBtn");
-bottomBtn= $("#bottomBtn");
+topBtn = $(".topBtn");
+bottomBtn= $(".bottomBtn");
 
-$(document).ready(function (){
+window.addEventListener("scroll", () => {
+    // console.log("scrolled!");
+    const scrolled = window.scrollY;
+    // console.log(scrolled);
+})
 
-    topBtn.on("click", function () {
-        function topFunction () {
-            document.documentElement.scrollTop = 0;
-        }
 
-        topFunction();
-    })
-
-    bottomBtn.on("click", function () {
-        function bottomFunction () {
-            document.documentElement.scrollTop = 26000;
-        }
-
-        bottomFunction();
-    })
-
+window.addEventListener("scroll", function () {
+    var scroll = document.querySelector(".topBtn");
+    scroll.classList.toggle("active", window.scrollY > 200)
 });
+
+window.addEventListener("scroll", function () {
+    var scroll = document.querySelector(".bottomBtn");
+    scroll.classList.toggle("active", window.scrollY > 5150)
+});
+
+topBtn.on("click", function () {
+    $("html, body").animate({scrollTop: 0}, "slow")
+});
+
+bottomBtn.on("click", () => {
+    $("html, body").animate({scrollTop: 12589}, "slow")
+});
+
+
+
+// $(document).ready (function (){
+//     $("#topBtn").hide();
+
+
+// });
+
+
+// $(window).scroll(function(){
+//     $('#topBtn').addClass('scrolled', $(this).scrolltop() > 10);
+// });
+
+
+// $(document).ready(function() {
+//     $("#mydiv").hide();
+//     $("#show").click(function() {
+//       $("#mydiv").toggle();
+//       $("#show").toggle();
+//     });
+//     $("#hide").click(function() {
+//       $("#mydiv").hide();
+//       $("#show").show();
+//     });
+//   });
+ 
+// $(document).ready(function (){
+
+//     topBtn.on("click", function () {
+//         function topFunction () {
+//             document.documentElement.scrollTop = 0;
+//         }
+
+//         topFunction();
+//     })
+
+//     bottomBtn.on("click", function () {
+//         function bottomFunction () {
+//             document.documentElement.scrollTop = 26000;
+//         }
+
+//         bottomFunction();
+//     })
+
+// });
+
+
 
 
 
