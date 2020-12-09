b627e71f3ef4f99f26b629ee7d78a0e0b3bc5290
Anthony Guerrero <adguerrero1935@gmail.com>
2020-12-08 23:55:14 -0500

    Cleaning up my code

--- assets/Scripts/btnFunction.js
+++ assets/Scripts/btnFunction.js
@@ -5,7 +5,7 @@ window.addEventListener("scroll", () => {
     // console.log("scrolled!");
     const scrolled = window.scrollY;
     // console.log(scrolled);
-})
+});
 
 
 window.addEventListener("scroll", function () {
@@ -25,27 +25,6 @@ topBtn.on("click",  () => {
 bottomBtn.on("click", () => {
     $("html, body").animate({scrollTop: 29589}, "slow")
 });
-
- 
-// $(document).ready(function (){
-
-//     topBtn.on("click", function () {
-//         function topFunction () {
-//             document.documentElement.scrollTop = 0;
-//         }
-
-//         topFunction();
-//     })
-
-//     bottomBtn.on("click", function () {
-//         function bottomFunction () {
-//             document.documentElement.scrollTop = 26000;
-//         }
-
-//         bottomFunction();
-//     })
-
-// });
 
 
 
